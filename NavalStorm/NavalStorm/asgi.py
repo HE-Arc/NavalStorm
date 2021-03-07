83f9b823a86d122b0271ee17892bad9e53a50be3
Paysant Adrien <adrien.paysant@he-arc.ch>
2021-03-07 21:46:29 +0100

    project creation with rest framework + add bootstrap + init config

--- NavalStorm/NavalStorm/asgi.py
+++ NavalStorm/NavalStorm/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for NavalStorm project.
+ASGI config for navalstorm project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NavalStorm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'navalstorm.settings')
 
 application = get_asgi_application()
