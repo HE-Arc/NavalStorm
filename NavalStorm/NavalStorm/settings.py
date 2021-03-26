eeb6f24d6110d6a0208f7822ac4809156f22c50e
Paysant Adrien <adrien.paysant@he-arc.ch>
2021-03-26 10:07:20 +0100

    add allowed host to deploy

--- NavalStorm/NavalStorm/settings.py
+++ NavalStorm/NavalStorm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECURITY_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['navalstorm.srvz-webapp.he-arc.ch']
 
 
 # Application definition
