0b0322498fd1ae9fd615d498d741c58bee285bb6
Paysant Adrien <adrien.paysant@he-arc.ch>
2021-03-26 12:01:49 +0100

    add host for local

--- NavalStorm/NavalStorm/settings.py
+++ NavalStorm/NavalStorm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECURITY_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['navalstorm.srvz-webapp.he-arc.ch']
+ALLOWED_HOSTS = ['navalstorm.srvz-webapp.he-arc.ch','127.0.0.1']
 
 
 # Application definition
