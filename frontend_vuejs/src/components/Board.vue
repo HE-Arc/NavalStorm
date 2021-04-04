670a9735ddd364ec3eddd22d98dc52f4f8f139c3
Faga Davide <faga.davide@gmail.com>
2021-04-04 09:49:09 +0200

    Add shadows preview position ships

--- frontend_vuejs/src/components/Board.vue
+++ frontend_vuejs/src/components/Board.vue
@@ -2,11 +2,8 @@
 <div id="app">
 <v-app>
 
-        
 
-
-
-  Selected: {{ selected }}
+ <h3>Selected: {{ selected }}</h3>
 
 
   <div class="container">
@@ -21,10 +18,10 @@
           <tr>
             <th v-for="y in boardHeader" :key="y">{{y}}</th>
           </tr>
-          <!-- battlefield board -->
+          <!-- BATTLEFIELD board -->
           <tr  v-for="x in boardSize" :key="x" >
             <th>{{x}}</th>
-            <td v-for="y in boardHeader" :key="y" :id="y+x"  @click="selectValue($event)"></td>
+            <td v-for="y in boardHeader" :key="y" :id="y+x" @click="putShipInBattleField($event)" @mouseleave="onAreaMouseLeave($event)" @mouseover="onAreaMouseOver($event)"></td>
           </tr>
         </table>
       </div>
@@ -32,18 +29,27 @@
         <!-- SHIPS in Harbor -->
         <div class="col-xs-3">
              <table>
-            <tr  v-for="ship in shipsInHarbor" :key="ship" >
-              <th>{{ship.type}}</th>
-              <td  :id="ship.id"  @click="selectValue($event)"><img :src="getImgUrl(ship.img)" alt="" width="200" height="40"></td>
+            <tr v-for="ship in shipsInHarbor" :key="ship" @click="selectShip(ship)">
+              <th >{{ship.type}}</th>
+              <td  :id="ship.id" @drop="onDropShipsInHarbor($event)" @dragenter.prevent @dragover.prevent>
+                <img  @dragstart="startDrag($event, ship)" :src="getImgUrl(ship.imgFront)" alt="" width="200" height="40">
+              </td>
+              <td>
+                <v-switch
+                :v-model="ship.isVertical"
+                :label="`is Vertical: ${ship.isVertical}`"
+                @click="onClickToggleVertical(ship)"
+              ></v-switch>
+              </td>
             </tr>
           </table>
         </div>
     </div>
 </div>
 
+    
 
-
-
+  <!--
 <div class="drop-zone" @drop="onDropShipsInHarbor($event)" @dragenter.prevent @dragover.prevent>
   <div v-for="ship in shipsInHarbor" :key="ship.id" class="drag-el" draggable="true" @dragstart="startDrag($event, ship)" > {{ ship.type }}</div>
 </div>
@@ -51,7 +57,7 @@
   <div v-for="ship in shipsInFleet" :key="ship.id" class="drag-el" draggable="true" @dragstart="startDrag($event, ship)" > {{ ship.type }}</div>
 </div>
 
-
+-->
 
 </v-app>
 
@@ -71,48 +77,63 @@ export default Vue.extend({
         selected: null,
         boardSize : 10,
         boardHeader : "",
-        board : [],
+        currentShip : null,
         shipsInFleet : [],
         shipsInHarbor : [
           {
             id : 1,
             isPlaced : false,
+            isVertical : false,
             type : "carrier",
             length : 5,
             coord : ["A2", "A3", "A4", "A5", "A6"],
-            img : "ships/carrier/Carrier_FrontView_2.png",
+            imgFront : "ships/carrier/Carrier_FrontView_2.png",
+            img : "ships/carrier/ShipCarrierHull.png",
+            imgV : "ships/carrier/ShipCarrierHull_V.png",
           },
           {
             id : 2,
             isPlaced : false,
+            isVertical : false,
             type : "battleship ",
-            lenght : 4,
+            length : 4,
             coord : ["I5", "I6", "I7", "I8"],
-            img : "ships/battleship/Battleship_FrontView_2.png"
+            imgFront : "ships/battleship/Battleship_FrontView_2.png",
+            img : "ships/battleship/ShipBattleshipHull.png",
+            imgV : "ships/battleship/ShipBattleshipHull_V.png",
           },
           {
             id : 3,
             isPlaced : false,
+            isVertical : false,
             type : "destroyer ",
-            lenght : 3,
+            length : 3,
             coord : ["C7", "C8", "C9"],
-            img : "ships/destroyer/Destroyer_FrontView_2.png",
+            imgFront : "ships/destroyer/Destroyer_FrontView_2.png",
+            img : "ships/destroyer/ShipDestroyerHull.png",
+            imgV : "ships/destroyer/ShipDestroyerHull_V.png",
           },
           {
             id : 4,
             isPlaced : false,
+            isVertical : false,
             type : "submarine",
-            lenght : 3,
+            length : 3,
             coord : ["D2", "E2", "F2"],
-            img : "ships/submarine/Submarine_FrontView_2.png",
+            imgFront : "ships/submarine/Submarine_FrontView_2.png",
+            img : "ships/submarine/ShipSubMarineHull.png",
+            imgV : "ships/submarine/ShipSubMarineHull_V.png",
           },
           {
             id : 5,
             isPlaced : false,
+            isVertical : false,
             type : "patrolboat",
-            lenght : 2,
+            length : 2,
             coord : ["E5", "F5"],
-            img : "ships/patrolBoat/PatrolBoat_FrontView_2.png",
+            imgFront : "ships/patrolBoat/PatrolBoat_FrontView_2.png",
+            img : "ships/patrolBoat/ShipPatrolHull.png",
+            imgV : "ships/patrolBoat/ShipPatrolHull_V.png",
           },
         ],
       }    
@@ -127,50 +148,138 @@ export default Vue.extend({
             id : String.fromCharCode(i) + j.toString(),
             isTouch : false,
             isBusy : false,
+            whoIsThere : null,
           }
           this.$store.dispatch('pushBoardValue', area);
         }
       }
-      console.log(this.$store.getters.getBoard);
     },
     methods: {
       getImgUrl(img) {
-           return require(`../assets/${img}`)
-      
+        return require(`../assets/${img}`) //i do not any chose to do that, else webpack don't interprete correctly the images
       },
-        selectValue (event) {
+      putShipInBattleField(event) {
+
+        if(this.currentShip === null)
+          return null;
+
+        var currentId = event.target.id;
+        var chars = currentId.slice(0, currentId.search(/\d/));
+        var numbs = currentId.replace(chars, '');
+        var l = parseInt(this.currentShip.length)
+        if(this.currentShip.isVertical)
+        { 
+          var n = parseInt(numbs)
+          for (let i = n; i < (n + l); i++) {
+            let coord = chars + i.toString();
+            let el = document.getElementById(coord)
+            el.style.backgroundColor = "green"
+          }
+        }
+        else
+        {
+          var c = chars.charCodeAt(0)
+          for (let i = c; i < (c + l); i++) {
+            let coord = String.fromCharCode(i) + numbs;
+            let el = document.getElementById(coord)
+            el.style.backgroundColor = "green"
+          }
+        }
+
+
+      },
+      selectValue (event) {
         this.selected = event.target.id;
         event.target.style.backgroundColor = 'green';
-        },
-        startDrag (event, ship) {
-          console.log(ship);
-          event.dataTransfer.dropEffect = 'move';
-          event.dataTransfer.effectAllowed = 'move';
-          event.dataTransfer.setData('shipID', ship.id);
-          event.target.style.backgroundColor = "red";
-          var img = document.createElement("img");
-          img.src = "https://people.he-arc.ch/photos/GrunenwaldDavid.jpg";
-          event.dataTransfer.setDragImage(img, 0, 0);
-        },
-        onDropShipsInFleet (event) {
-          const shipID = event.dataTransfer.getData('shipID');
-          const ship = this.shipsInHarbor.find((ship) => ship.id == shipID);
-          if (typeof ship !== 'undefined') {
-            const shipIndex = this.shipsInHarbor.indexOf(ship);
-            this.shipsInHarbor.splice(shipIndex, 1);
-            this.shipsInFleet.push(ship);
-          }          
-        },
-        onDropShipsInHarbor (event) {
-          const shipID = event.dataTransfer.getData('shipID');
-          const ship = this.shipsInFleet.find((ship) => ship.id == shipID);
-          if (typeof ship !== 'undefined') {
-            const shipIndex = this.shipsInFleet.indexOf(ship);
-            this.shipsInFleet.splice(shipIndex, 1);
-            this.shipsInHarbor.push(ship);
+      },
+      selectShip (ship) {
+        this.currentShip = ship;
+        this.selected = ship.type;
+      },
+      startDrag (event, ship) {
+        event.dataTransfer.dropEffect = 'move';
+        event.dataTransfer.effectAllowed = 'move';
+        event.dataTransfer.setData('shipID', ship.id);
+        var img = document.createElement("img");
+        img.src = this.getImgUrl(ship.img);//ship.img;//"https://people.he-arc.ch/photos/GrunenwaldDavid.jpg";
+        event.dataTransfer.setDragImage(img, 0, 0);
+      },
+      onDropShipsInFleet (event) {
+        const shipID = event.dataTransfer.getData('shipID');
+        const ship = this.shipsInHarbor.find((ship) => ship.id == shipID);
+        if (typeof ship !== 'undefined') {
+          const shipIndex = this.shipsInHarbor.indexOf(ship);
+          this.shipsInHarbor.splice(shipIndex, 1);
+          this.shipsInFleet.push(ship);
+        }          
+      },
+      onDropShipsInHarbor (event) {
+        const shipID = event.dataTransfer.getData('shipID');
+        const ship = this.shipsInFleet.find((ship) => ship.id == shipID);
+        if (typeof ship !== 'undefined') {
+          const shipIndex = this.shipsInFleet.indexOf(ship);
+          this.shipsInFleet.splice(shipIndex, 1);
+          this.shipsInHarbor.push(ship);
+        }
+      },
+      onClickToggleVertical(ship){
+        ship.isVertical = !ship.isVertical;
+      },
+      onAreaMouseOver(event) {
+        var currentId = event.target.id;
+        var chars = currentId.slice(0, currentId.search(/\d/));
+        var numbs = currentId.replace(chars, '');
+        var l = parseInt(this.currentShip.length)
+        if(this.currentShip.isVertical)
+        { 
+          var n = parseInt(numbs)
+          if((l + n) > (this.boardSize+1))
+            return null;
+         
+          for (let i = n; i < (n + l); i++) {
+            let coord = chars + i.toString();
+            let el = document.getElementById(coord)
+            el.style.backgroundColor = "green"
+          }
+        }
+        else
+        {
+          var c = chars.charCodeAt(0)
+          if((c-65 +1 +l) > (this.boardSize +1))
+            return null;
+
+          for (let i = c; i < (c + l); i++) {
+            let coord = String.fromCharCode(i) + numbs;
+            let el = document.getElementById(coord)
+            el.style.backgroundColor = "green"
           }
         }
       },
+      onAreaMouseLeave(event) {
+        var currentId = event.target.id;
+        var chars = currentId.slice(0, currentId.search(/\d/));
+        var numbs = currentId.replace(chars, '');
+        var l = parseInt(this.currentShip.length)
+        if(this.currentShip.isVertical)
+        { 
+          var n = parseInt(numbs)
+          for (let i = n; i < (n + l); i++) {
+            let coord = chars + i.toString();
+            let el = document.getElementById(coord)
+            el.style.backgroundColor = "#1e8fff21"
+          }
+        }
+        else
+        {
+          var c = chars.charCodeAt(0)
+          for (let i = c; i < (c + l); i++) {
+            let coord = String.fromCharCode(i) + numbs;
+            let el = document.getElementById(coord)
+            el.style.backgroundColor ="#1e8fff21"
+          }
+        }
+      },
+    },
     computed: {
     },
 });
