4010e9e1207deb4442fcbd940d4af78eabbd00df
Faga Davide <faga.davide@gmail.com>
2021-04-05 17:06:48 +0200

    Improve Board, manage input from gamer

--- frontend_vuejs/src/components/Board.vue
+++ frontend_vuejs/src/components/Board.vue
@@ -2,15 +2,18 @@
 <div id="app">
 <v-app>
 
-
- <h3>Selected: {{ selected }}</h3>
-
-
+<h3 v-if="currentShip == null">
+  Please Selecte a ship
+</h3>
+<h3 v-else>
+  Selected: {{ currentShip.type }} 
+</h3>
+ 
   <div class="container">
     <div class="row">
       <div class="col-xs-3">
           <!-- BOARD -->
-        <table background="https://cdn.hipwallpaper.com/i/67/75/zxPV0f.gif">
+        <table :background="getImgUrl('lands/sea.gif')">
             <!-- header board -->
           <tr >
             <td rowspan="2"></td>
@@ -19,7 +22,7 @@
             <th v-for="y in boardHeader" :key="y">{{y}}</th>
           </tr>
           <!-- BATTLEFIELD board -->
-          <tr  v-for="x in boardSize" :key="x" >
+           <tr  v-for="x in boardSize" :key="x" >
             <th>{{x}}</th>
             <td v-for="y in boardHeader" :key="y" :id="y+x" @click="putShipInBattleField($event)" @mouseleave="onAreaMouseLeave($event)" @mouseover="onAreaMouseOver($event)"></td>
           </tr>
@@ -29,38 +32,30 @@
         <!-- SHIPS in Harbor -->
         <div class="col-xs-3">
              <table>
-            <tr v-for="ship in shipsInHarbor" :key="ship" @click="selectShip(ship)">
+            <tr v-for="ship in this.$store.getters.getShips" :key="ship" @click="selectShip(ship)">
               <th >{{ship.type}}</th>
-              <td  :id="ship.id" @drop="onDropShipsInHarbor($event)" @dragenter.prevent @dragover.prevent>
-                <img  @dragstart="startDrag($event, ship)" :src="getImgUrl(ship.imgFront)" alt="" width="200" height="40">
+              <td>
+                <img :src="getImgUrl(ship.imgFront)" alt="" width="200" height="40">
               </td>
               <td>
-                <v-switch
-                :v-model="ship.isVertical"
-                :label="`is Vertical: ${ship.isVertical}`"
-                @click="onClickToggleVertical(ship)"
-              ></v-switch>
+                <label>
+                  <input type="checkbox" v-if="ship.isPlaced" disabled="true"/>
+                  <input type="checkbox" v-else @click="onClickToggleVertical(ship)" v-model="ship.isVertical"/>
+                  Vertical
+                </label>
+              <td :id="ship.id">
+               <label>
+                  <input type="checkbox" v-if="!ship.isPlaced" disabled="true" />
+                  <input type="checkbox" v-else @click="onClickTogglePlaced(ship)" v-model="ship.isPlaced"/>
+                  Placed
+               </label>
               </td>
             </tr>
           </table>
         </div>
     </div>
 </div>
-
-    
-
-  <!--
-<div class="drop-zone" @drop="onDropShipsInHarbor($event)" @dragenter.prevent @dragover.prevent>
-  <div v-for="ship in shipsInHarbor" :key="ship.id" class="drag-el" draggable="true" @dragstart="startDrag($event, ship)" > {{ ship.type }}</div>
-</div>
-<div class="drop-zone" @drop="onDropShipsInFleet($event)" @dragenter.prevent @dragover.prevent>
-  <div v-for="ship in shipsInFleet" :key="ship.id" class="drag-el" draggable="true" @dragstart="startDrag($event, ship)" > {{ ship.type }}</div>
-</div>
-
--->
-
 </v-app>
-
 </div>
 </template>
 
@@ -74,68 +69,9 @@ export default Vue.extend({
     name: "Board",
     data ()  { 
     return  {
-        selected: null,
         boardSize : 10,
         boardHeader : "",
         currentShip : null,
-        shipsInFleet : [],
-        shipsInHarbor : [
-          {
-            id : 1,
-            isPlaced : false,
-            isVertical : false,
-            type : "carrier",
-            length : 5,
-            coord : ["A2", "A3", "A4", "A5", "A6"],
-            imgFront : "ships/carrier/Carrier_FrontView_2.png",
-            img : "ships/carrier/ShipCarrierHull.png",
-            imgV : "ships/carrier/ShipCarrierHull_V.png",
-          },
-          {
-            id : 2,
-            isPlaced : false,
-            isVertical : false,
-            type : "battleship ",
-            length : 4,
-            coord : ["I5", "I6", "I7", "I8"],
-            imgFront : "ships/battleship/Battleship_FrontView_2.png",
-            img : "ships/battleship/ShipBattleshipHull.png",
-            imgV : "ships/battleship/ShipBattleshipHull_V.png",
-          },
-          {
-            id : 3,
-            isPlaced : false,
-            isVertical : false,
-            type : "destroyer ",
-            length : 3,
-            coord : ["C7", "C8", "C9"],
-            imgFront : "ships/destroyer/Destroyer_FrontView_2.png",
-            img : "ships/destroyer/ShipDestroyerHull.png",
-            imgV : "ships/destroyer/ShipDestroyerHull_V.png",
-          },
-          {
-            id : 4,
-            isPlaced : false,
-            isVertical : false,
-            type : "submarine",
-            length : 3,
-            coord : ["D2", "E2", "F2"],
-            imgFront : "ships/submarine/Submarine_FrontView_2.png",
-            img : "ships/submarine/ShipSubMarineHull.png",
-            imgV : "ships/submarine/ShipSubMarineHull_V.png",
-          },
-          {
-            id : 5,
-            isPlaced : false,
-            isVertical : false,
-            type : "patrolboat",
-            length : 2,
-            coord : ["E5", "F5"],
-            imgFront : "ships/patrolBoat/PatrolBoat_FrontView_2.png",
-            img : "ships/patrolBoat/ShipPatrolHull.png",
-            imgV : "ships/patrolBoat/ShipPatrolHull_V.png",
-          },
-        ],
       }    
     }, 
     mounted(){
@@ -150,30 +86,67 @@ export default Vue.extend({
             isBusy : false,
             whoIsThere : null,
           }
-          this.$store.dispatch('pushBoardValue', area);
+          this.$store.dispatch('pushAreaInBoard', area);
         }
       }
     },
     methods: {
+      //webpack don't interpret images correctly without going through this solution
       getImgUrl(img) {
-        return require(`../assets/${img}`) //i do not any chose to do that, else webpack don't interprete correctly the images
+        return require(`../assets/${img}`) 
       },
       putShipInBattleField(event) {
-
-        if(this.currentShip === null)
+        //controle if current ship is valide
+        if(this.currentShip === null) {
+          let h3 = document.getElementsByTagName("h3")[0]
+          h3.style.background = 'pink'
+          setTimeout(()=>{
+            h3.style.background = 'white'
+          },1000)
           return null;
+        }
+          
+        //controle if the current ship already in the battelfield
+        if(this.currentShip.isPlaced) {
+         // alert(this.currentShip.type+" already on the battlefield")
+         let el = document.getElementById(this.currentShip.id)
+          el.style.background = 'pink'
+          setTimeout(()=>{
+            el.style.background = 'white'
+          },1000)
+          this.currentShip = null
+          return null
+        }
 
+        //parse id
         var currentId = event.target.id;
         var chars = currentId.slice(0, currentId.search(/\d/));
         var numbs = currentId.replace(chars, '');
         var l = parseInt(this.currentShip.length)
+
         if(this.currentShip.isVertical)
         { 
           var n = parseInt(numbs)
+          //controle if one of them is busy
+          for (let i = n; i < (n + l); i++) {
+            let coord = chars + i.toString();    
+            if(this.$store.getters.getBoardAreaById(coord).isBusy) {
+              return null;
+            }
+          }
+          //update Area + ship
           for (let i = n; i < (n + l); i++) {
             let coord = chars + i.toString();
+            this.currentShip.coord.push(coord)
+            let area = { 
+              id : coord,
+              isTouch : false,
+              isBusy : true,
+              whoIsThere : this.currentShip.id,
+            }
+            this.$store.dispatch('updateAreaInBoard', area);
             let el = document.getElementById(coord)
-            el.style.backgroundColor = "green"
+            el.style.backgroundColor = this.currentShip.color
           }
         }
         else
@@ -181,51 +154,59 @@ export default Vue.extend({
           var c = chars.charCodeAt(0)
           for (let i = c; i < (c + l); i++) {
             let coord = String.fromCharCode(i) + numbs;
+            if(this.$store.getters.getBoardAreaById(coord).isBusy) {
+              return null;
+            }
+          }
+          for (let i = c; i < (c + l); i++) {
+            let coord = String.fromCharCode(i) + numbs;
+            this.currentShip.coord.push(coord)
+            let area = { 
+              id : coord,
+              isTouch : false,
+              isBusy : true,
+              whoIsThere : this.currentShip.id,
+            }
+            this.$store.dispatch('updateAreaInBoard', area);
             let el = document.getElementById(coord)
-            el.style.backgroundColor = "green"
+            el.style.backgroundColor = this.currentShip.color
           }
         }
-
-
-      },
-      selectValue (event) {
-        this.selected = event.target.id;
-        event.target.style.backgroundColor = 'green';
+        this.currentShip.isPlaced = true;
+        this.$store.dispatch('updateShipInShips', this.currentShip)
+        this.currentShip = null
       },
       selectShip (ship) {
-        this.currentShip = ship;
-        this.selected = ship.type;
+        this.currentShip = this.$store.getters.getShipById(ship.id);
       },
-      startDrag (event, ship) {
-        event.dataTransfer.dropEffect = 'move';
-        event.dataTransfer.effectAllowed = 'move';
-        event.dataTransfer.setData('shipID', ship.id);
-        var img = document.createElement("img");
-        img.src = this.getImgUrl(ship.img);//ship.img;//"https://people.he-arc.ch/photos/GrunenwaldDavid.jpg";
-        event.dataTransfer.setDragImage(img, 0, 0);
-      },
-      onDropShipsInFleet (event) {
-        const shipID = event.dataTransfer.getData('shipID');
-        const ship = this.shipsInHarbor.find((ship) => ship.id == shipID);
-        if (typeof ship !== 'undefined') {
-          const shipIndex = this.shipsInHarbor.indexOf(ship);
-          this.shipsInHarbor.splice(shipIndex, 1);
-          this.shipsInFleet.push(ship);
-        }          
-      },
-      onDropShipsInHarbor (event) {
-        const shipID = event.dataTransfer.getData('shipID');
-        const ship = this.shipsInFleet.find((ship) => ship.id == shipID);
-        if (typeof ship !== 'undefined') {
-          const shipIndex = this.shipsInFleet.indexOf(ship);
-          this.shipsInFleet.splice(shipIndex, 1);
-          this.shipsInHarbor.push(ship);
+      onClickTogglePlaced(ship){
+        ship.isPlaced = !ship.isPlaced
+        if(!ship.isPlaced)
+        {
+          ship.coord.forEach((co) => { 
+            let currentArea = this.$store.getters.getBoardAreaById(co)
+            currentArea.isBusy = false
+            currentArea.whoIsThere = null
+            this.$store.dispatch('updateAreaInBoard', currentArea)
+          })
+          ship.coord = []
+          this.$store.dispatch('updateShipInShips', ship)
+
+          this.updateBoardHTML()
         }
+
       },
       onClickToggleVertical(ship){
         ship.isVertical = !ship.isVertical;
       },
       onAreaMouseOver(event) {
+         //controle if current ship is valide
+        if(this.currentShip === null)
+          return null;
+        
+        if(this.currentShip.isPlaced)
+          return null;
+
         var currentId = event.target.id;
         var chars = currentId.slice(0, currentId.search(/\d/));
         var numbs = currentId.replace(chars, '');
@@ -256,26 +237,21 @@ export default Vue.extend({
         }
       },
       onAreaMouseLeave(event) {
-        var currentId = event.target.id;
-        var chars = currentId.slice(0, currentId.search(/\d/));
-        var numbs = currentId.replace(chars, '');
-        var l = parseInt(this.currentShip.length)
-        if(this.currentShip.isVertical)
-        { 
-          var n = parseInt(numbs)
-          for (let i = n; i < (n + l); i++) {
-            let coord = chars + i.toString();
-            let el = document.getElementById(coord)
-            el.style.backgroundColor = "#1e8fff21"
+        event.target.id
+        this.updateBoardHTML()
+      },
+      updateBoardHTML() {
+        var currentBoard = this.$store.getters.getBoard
+        for (var i = 0; i < currentBoard.length; i++) {
+          var currentArea = currentBoard[i]
+          var el = document.getElementById(currentArea.id)
+
+          if(currentArea.isBusy){ 
+            let ship = this.$store.getters.getShipById(currentArea.whoIsThere)
+            el.style.backgroundColor = ship.color
           }
-        }
-        else
-        {
-          var c = chars.charCodeAt(0)
-          for (let i = c; i < (c + l); i++) {
-            let coord = String.fromCharCode(i) + numbs;
-            let el = document.getElementById(coord)
-            el.style.backgroundColor ="#1e8fff21"
+          else{
+            el.style.backgroundColor = "#1e8fff21"
           }
         }
       },
--- frontend_vuejs/src/store/index.js
+++ frontend_vuejs/src/store/index.js
@@ -5,23 +5,109 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-     board: [],
+    board: [],
+    ships : [
+      {
+        id : 1,
+        isPlaced : false,
+        isVertical : false,
+        type : "carrier",
+        length : 5,
+        coord : [],
+        imgFront : "ships/carrier/Carrier.png",
+        color : "Coral",
+      },
+      {
+        id : 2,
+        isPlaced : false,
+        isVertical : false,
+        type : "battleship ",
+        length : 4,
+        coord : [],
+        imgFront : "ships/battleship/Battleship.png",
+        color : "Sienna",
+      },
+      {
+        id : 3,
+        isPlaced : false,
+        isVertical : false,
+        type : "destroyer ",
+        length : 3,
+        coord : [],
+        imgFront : "ships/destroyer/Destroyer.png",
+        color : "Peru",
+      },
+      {
+        id : 4,
+        isPlaced : false,
+        isVertical : false,
+        type : "submarine",
+        length : 3,
+        coord : [],
+        imgFront : "ships/submarine/Submarine.png",
+        color : "MediumAquaMarine",
+      },
+      {
+        id : 5,
+        isPlaced : false,
+        isVertical : false,
+        type : "patrolboat",
+        length : 2,
+        coord : [],
+        imgFront : "ships/patrolBoat/PatrolBoat.png",
+        color : "DarkOrchid",
+      },
+    ],
   },
-   getters: {
+  getters: {
+    getShips: (state) => {
+      return state.ships;
+    },
+    getShipByType: (state) => (shipType) => {
+      return state.ships.find(s => s.type === shipType);
+    },
+    getShipById: (state) => (shipID) => {
+      return state.ships.find(ship => ship.id === shipID);
+    },
     getBoard: (state) => {
       return state.board;
-    }
+    },
+    getBoardAreaById: (state) => (areaID) => {
+      return state.board.find(ar => ar.id === areaID);
+    },
   },
   mutations: {
-    PUSH_BOARD_VALUE:(state, value) => {
-      state.board.push(value);
+    PUSH_BOARD_AREA:(state, area) => {
+      state.board.push(area);
+    },
+    UPDATE_BOARD_AREA:(state, newArea) => {
+      const oldArea = state.board.find((oldArea) => oldArea.id === newArea.id);
+      const oldAreaIndex = state.board.indexOf(oldArea);
+      state.board.splice(oldAreaIndex, 1);
+      state.board.push(newArea);
+    },
+    PUSH_SHIPS_SHIP:(state, ship) => {
+      state.ships.push(ship);
+    },
+    UPDATE_SHIPS_SHIP:(state, newShip) => {
+      const oldShip = state.ships.find((oldShip) => oldShip.id === newShip.id);
+      const oldShipIndex = state.ships.indexOf(oldShip);
+      state.ships.splice(oldShipIndex, 1);
+      state.ships.push(newShip);
     },
   },
   actions: {
-      pushBoardValue : (store, value) => {
-        store.commit('PUSH_BOARD_VALUE', value);
+    pushAreaInBoard : (store, area) => {
+      store.commit('PUSH_BOARD_AREA', area);
+    },
+    updateAreaInBoard : (store, area) => {
+      store.commit('UPDATE_BOARD_AREA', area);
+    },
+    pushShipInShips : (store, ship) => {
+      store.commit('PUSH_SHIPS_SHIP', ship)
+    },
+    updateShipInShips : (store, ship) => {
+      store.commit('UPDATE_SHIPS_SHIP', ship);
     },
   },
-  modules: {
-  }
 })
