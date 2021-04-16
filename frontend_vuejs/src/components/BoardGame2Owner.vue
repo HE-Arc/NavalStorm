ef712fe20344df6020a7633f211850372b85cbb4
Faga Davide <faga.davide@gmail.com>
2021-04-16 17:11:51 +0200

    Add phase2 frontend

--- frontend_vuejs/src/components/BoardGame2Owner.vue
+++ frontend_vuejs/src/components/BoardGame2Owner.vue
@@ -14,11 +14,11 @@
     <!-- BATTLEFIELD board -->
     <tr  v-for="x in $store.getters.getBoardSize" :key="x" >
       <th>{{x}}</th>
-      <td v-for="y in $store.getters.getBoardHeader" :key="y" :id="y+x"></td>
+      <td v-for="y in $store.getters.getBoardHeader" :key="y" :id="prefixID+y+x">
+
+      </td>
     </tr>
   </table>
- <div id="sprite-image">
-  </div>
 </v-app>
 </div>
 </template>
@@ -31,34 +31,54 @@ export default Vue.extend({
     name: "BoardGame2Owner",
     data ()  { 
     return  {
-      animationInterval: null,
-      spriteSheet: null,
-      widthOfSpriteSheet: 1800,
-      widthOfEachSprite: 40,
-      heightOfSpriteSheet: 100,
-      heightOfEachSprite: 100,
-
+      prefixID : this.$options.name,
       }    
     },
     created() {
-      
-
+      // this make a global ref, nice to call methode from other component
+      this.$root.$refs.BoardGame2Owner = this;
     },
     mounted(){
       this.updateBoardHTML()
-      this.startAnimation()
-      this.spriteSheet = document.getElementById("sprite-image")
     },
     methods: {
       //webpack don't interpret images correctly without going through this solution
       getImgUrl(img) {
         return require(`../assets/${img}`) 
       },
+      attaque(id) {console.log(id)
+        //refresh board
+        var currentId = this.prefixID+id
+        var coordId = id 
+        console.log(currentId)
+        var td = document.getElementById(currentId)
+        if (td.childElementCount != 0 && td != null) //allow attaque only if it's the first time
+          return;
+
+        var img = document.createElement('img');
+        var area = this.$store.getters.getBoardAreaById(coordId)
+        if(area.isBusy)
+          img.src = this.getImgUrl("explosions/explosion-ico.png")
+        else
+          img.src = this.getImgUrl("explosions/wather-splash-ico.png")
+
+        area.isTouch = true
+        this.$store.dispatch('updateAreaInBoard', area);
+
+        //style the img
+        img.style.height = "20px"
+        img.style.width = "20px"
+        img.style.padding = "0px"
+        img.style.margin = "0px"
+        img.style.display = "block"
+        td.insertAdjacentElement("beforeend", img);
+
+      },
       updateBoardHTML() {
         var currentBoard = this.$store.getters.getBoard
         for (var i = 0; i < currentBoard.length; i++) {
           var currentArea = currentBoard[i]
-          var el = document.getElementById(currentArea.id)
+          var el = document.getElementById(this.prefixID+currentArea.id)
 
           if(currentArea.isBusy){ 
             let ship = this.$store.getters.getShipById(currentArea.whoIsThere)
@@ -69,31 +89,6 @@ export default Vue.extend({
           }
         }
       },
-      //Animation
-      stopAnimation() {
-        clearInterval(this.animationInterval)
-      },
-      startAnimation() {
-        var position = this.widthOfEachSprite; //start position for the image
-        const speed = 100; //in millisecond(ms)
-        const diff = this.widthOfEachSprite; //difference between two sprites
-       
-        
-        this.animationInterval = setInterval(() => {
-          this.spriteSheet.style.backgroundPosition = `-${position}px 0px`;
-
-          if (position < this.widthOfSpriteSheet) 
-          {
-            position = position + diff;
-          } 
-          else 
-          {
-            //increment the position by the width of each sprite each time
-            position = this.widthOfEachSprite;
-          }
-        //reset the position to show first sprite after the last one
-        }, speed);
-    },
     },
 });
 </script>
