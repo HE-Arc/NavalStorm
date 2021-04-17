2af9b04b8aceaa7c5702e9178b1826218738dac9
Monnet Joris <joris.monnet@he-arc.ch>
2021-04-17 21:31:08 +0200

    begin to work on serializers and models of the servers

--- navalstorm/game/models.py
+++ navalstorm/game/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from user.models import NavalStormUser
 
 # Create your models here.
+
+class Servers(models.Model):
+    name = models.CharField(Blank=False, max_length="50")
+    password = models.CharField(Blank=True,max_length="255")
+    first_player = models.ForeignKey(NavalStormUser, on_delete="SET_NULL")
+    second_player = models.ForeignKey(NavalStormUser, on_delete="SET_NULL")
+
+    @classmethod
+    def create_navalstorm_server(cls, first_player, name, password):
+        server = cls()
+        server.first_player = first_player
+        server.name = name
+        server.password = password
+        server.save()
+        return server
+
+    def check_password(self,password):
+        return True #TODO VERIFICATION OF PASSWORD
+        return password == self.password
+    
+    def addPlayer(self,player):
+        self.second_player = player
+        self.save()
+        return self
+
+    def isFull(self):
+        return self.first_player and self.second_player
---
+++ navalstorm/game/serializers.py
@@ -0,0 +1,59 @@
+from rest_framework import serializers
+from rest_framework.response import Response
+from .models import Servers
+from user.models import NavalStormUser
+
+class ServerCreaterSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(Required=False)
+    player_id = serializers.IntegerField() #required
+    
+    class Meta:
+        model = Servers
+        fields = ['player']
+
+    def create(self, validated_data):
+        try:
+            player = NavalStormUser.objects.get(id=validated_data['player_id'])
+        except  NavalStormUser.DoesNotExist:
+            pass #TODO handle the case when the user does not exist.
+
+        password = validated_data['password']
+        name = validated_data['name']
+        return Servers.create_navalstorm_server(player,name,password)
+
+    def connect(self, validated_data):
+        if not validated_data['name']:
+            pass #random connexion without password 
+
+        try:
+            server = Servers.objects.get(name=validated_data['name'])
+        except Servers.DoesNotExist:
+            pass #TODO RESPONSE = NO SERVER OF THIS NAME
+
+        if server.isFull():
+            try:
+                player = NavalStormUser.objects.get(id=validated_data['player_id'])
+            except  NavalStormUser.DoesNotExist:
+                pass #TODO handle the case when the user does not exist.
+            if server.checkPlayers(player):
+                return #TODO RESPONSE = CONNEXION
+            return #TODO RESPONSE = IS FULL
+
+        if server.check_password(validated_data['password']) :
+            try:
+                player = NavalStormUser.objects.get(id=validated_data['player_id'])
+            except  NavalStormUser.DoesNotExist:
+                pass #TODO handle the case when the user does not exist.
+            server.addPlayer(player)
+        else :
+            pass #TODO RESPONSE to say password isn't good
+
+class ServerConnecterSerializer(serializers.Serializer)
+    class Meta:
+        model = Servers
+        fields = ['second_player','name','password']
+
+class ServerSerializer(serializers.Serializer):   
+    class Meta:
+        model = Servers
+        fields = '__all__'
