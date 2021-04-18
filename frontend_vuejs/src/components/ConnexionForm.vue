30afee32a993b37518b6a54435ffee515ffbc6bf
Monnet Joris <joris.monnet@he-arc.ch>
2021-04-18 14:06:20 +0200

    begin to work on frontend page connexion : issue #22

---
+++ frontend_vuejs/src/components/ConnexionForm.vue
@@ -0,0 +1,25 @@
+<!-- TEMPLATE -->
+<template>
+    <div id="app">
+        <v-app>
+
+        </v-app>
+    </div>
+</template>
+
+<!-- SCRIPT -->
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+    name: "ConnexionForm", 
+    methods: {
+        randomConnexion(){
+
+        },
+        normalConnexion(){
+
+        },
+    },
+});
+</script>
---
+++ frontend_vuejs/src/views/Connexion.vue
@@ -0,0 +1,20 @@
+<template>
+<v-container class="marginTop">
+    <div class="container">
+        <div class="">
+          <ConnexionForm />
+        </div>
+    </div>
+</v-container>
+</template>
+
+<script>
+import ConnexionForm from '@/components/ConnexionForm.vue'
+
+export default {
+  name: 'Connexion',
+  components: {
+    ConnexionForm,
+  }
+}
+</script>
